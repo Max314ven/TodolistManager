70fa67635446a241ec3d1067529c93c4b82fef7f
Max <maksympiven94@gmail.com>
2021-02-10 23:04:53 +0100

    ADDED: - tasksArr state - removeTask button - activeFilter state - changing filter buttons ("All","Active","Done")

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./App.module.css";
 import {TodoList} from "./TodoList";
 
+type TodoListType = {
+    title: string
+}
+
+export type TasksArrType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}[]
 function App() {
+
+    let todoList: TodoListType = {
+        title: "First Todo",
+    }
+
+    let [tasksArr, setTasksArr] = useState(
+        [{
+            id: 1,
+            title: "Learn JS",
+            isDone: true
+        },
+            {
+                id: 2,
+                title: "Learn React",
+                isDone: true
+            },
+            {
+                id: 3,
+                title: "Code practice",
+                isDone: false
+            },
+            {
+                id: 4,
+                title: "Learn English",
+                isDone: false
+            }
+        ]
+    )
+    let[activeFilter, setActiveFilter]=useState<"All"|"Active"|"Done">("All")
+
+    let tasksForTodoList = tasksArr
+
+    if(activeFilter === "Active"){
+        tasksForTodoList = tasksForTodoList.filter(task=>!task.isDone)
+    }
+    if(activeFilter === "Done"){
+        tasksForTodoList = tasksForTodoList.filter(task=>task.isDone)
+    }
+
+    function changeActiveFilter (value:"All"|"Active"|"Done"){
+        setActiveFilter(value)
+    }
+
+    function removeTask (taskID:number){
+        let filteredTask= tasksArr.filter(t=> t.id !==taskID)
+        setTasksArr(filteredTask)
+    }
+
+
     return (
         <div className={s.app_container}>
-            <TodoList/>
+            <TodoList title={todoList.title}
+                      tasksArr={tasksForTodoList}
+                      removeTask={removeTask}
+                      changeActiveFilter={changeActiveFilter}
+            />
         </div>
 
     )
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import s from "./Todolist.module.css"
+import {TasksArrType} from "./App";
 
-export function TodoList() {
+type TodoListType={
+    title:string
+    tasksArr:TasksArrType
+    removeTask:(taskID:number)=>void
+    changeActiveFilter:(value:"All"|"Active"|"Done")=>void
+}
+
+export function TodoList(props:TodoListType) {
+    const onAllClickHandler=()=>{props.changeActiveFilter("All")}
+    const onActiveClickHandler=()=>{props.changeActiveFilter("Active")}
+    const onCompletedClickHandler=()=>{props.changeActiveFilter("Done")}
+
+
+
+
     return (
         <div className={s.todolist_container}>
             <div className={s.todolist_container__header}>
                 <div className={s.todolist_container__header_title}>
-                    <h3>First Todolist</h3>
+                    <h3>{props.title}</h3>
                 </div>
                 <div className={s.todolist_container__header_input}>
                     <input type="text" placeholder="Type new task here..."/>
@@ -15,23 +30,23 @@ export function TodoList() {
             </div>
             <div className={s.todolist_container__list}>
                 <ul>
-                    <li>
-                        <input type="checkbox"/>
-                        <span>TaskName</span>
-                        <button>🗙</button>
-                    </li>
-                    <li>
-                        <input type="checkbox"/>
-                        <span>TaskName</span>
-                        <button>🗙</button>
-                    </li>
-                    <li>
-                        <input type="checkbox"/>
-                        <span>TaskName TaskName TaskName TaskName TaskName TaskName TaskName TaskName TaskName TaskName TaskName TaskName  </span>
-                        <button>🗙</button>
-                    </li>
+                    {
+                        props.tasksArr.map(task=>{
+                            return(
+                                <li key={task.id}>
+                                    <input type="checkbox" checked={task.isDone}/>
+                                    <span>{task.title}</span>
+                                    <button onClick={()=>props.removeTask(task.id)}>🗙</button>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
-
+            </div>
+            <div className={s.todolist_container__filters}>
+                <button onClick={onAllClickHandler}>All</button>
+                <button onClick={onActiveClickHandler}>Active</button>
+                <button onClick={onCompletedClickHandler}>Done</button>
             </div>
         </div>
     )
